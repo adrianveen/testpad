b421c2cda0f5dbc04f106ee937a2dbebed4f1cc5
Adrian <adrian.veen@gmail.com>
2025-02-25 17:37:09 -0500

    added fft/timedomain plotting for sweep files

--- sweep_plotter/sweep_graph.py
+++ sweep_plotter/sweep_graph.py
@@ -2,12 +2,137 @@
 import h5py
 import numpy as np
 import os
+import sys
 from numpy import fft as fft
 from pprint import pprint
 import pandas as pd
+from PIL import Image
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import matplotlib.ticker as ticker
+from matplotlib.colors import to_rgb, to_hex
+from matplotlib.backends.backend_qtagg import FigureCanvas
 
-class FFTGraph():
-    _
+class SweepGraph():
+    def __init__(self, hdf5_file, time_delta=16e-9):
+        
+        self.hdf5_file = hdf5_file
+        self.scan_data = []
+        self.time_delta = time_delta
+        # check if hdf5_file is None
+        if hdf5_file is None:
+            raise ValueError("No file selected")
+        
+        # process the file and store the data
+        self.raw_data = self._process_files(hdf5_file)
+
+    def resource_path(self, relative_path):
+        """Get the absolute path to a resource"""
+        base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+        return os.path.join(base_path, relative_path)
+
+    def load_icon(self, path):
+        image = Image.open(path)
+        image_array = np.array(image)
+        return image_array
+    # Generate a color palette based on the base color provided
+    def generate_color_palette(self, base_color, num_colors):
+        base_rgb = to_rgb(base_color)
+        palette = [to_hex((base_rgb[0] * (1 - i / num_colors), 
+                           base_rgb[1] * (1 - i / num_colors), 
+                           base_rgb[2] * (1 - i / num_colors))) for i in range(num_colors)]
+        return palette
+    
+    def _process_files(self, file_paths):
+        """
+        Process one or more CSV files and extract relevant data.
+        
+        Args:
+            file_paths (str or list of str): Single file path or a list of file paths.
+            
+        Returns:
+            list: A list of tuples, where each tuple contains (elapsed time, temperature).
+        """
+        # Ensure file_paths is always a list
+        if isinstance(file_paths, str):
+            file_paths = [file_paths]
+
+        for file_path in file_paths:
+            try:
+                with h5py.File(file_path, 'r') as f:
+                    # Access the Scan group
+                    scan_group = f['Scan']
+                    
+                    # Extract the raw pressure waveform data
+                    raw_pressure_waveforms = scan_group['Raw pressure waveforms (Pa)'][:]
+                    
+                    # Print the shape of the raw data (optional)
+                    print(f"File: {file_path} - Data shape: {raw_pressure_waveforms.shape}")
+                    
+                    # Convert the data into a pandas DataFrame
+                    df = pd.DataFrame(raw_pressure_waveforms)
+                    
+                    # Append the DataFrame to raw_data
+                    self.scan_data.append(df)
+                    print(f"Raw data: {self.scan_data[0].shape[1]}")
+            except Exception as e:
+                print(f"Error processing file {file_path}: {e}")
+
+        return self.scan_data
+    
+    def get_graphs(self, trace_index, graph_type='time'):
+        """
+        Generate a graph based on the selected trace number and graph type.
+        
+        Args:
+            trace_no (int): The trace number to plot.
+            graph_type (str): The type of graph to plot. Options are 'time' or 'fft'.
+            
+        Returns:
+            matplotlib.figure.Figure: A matplotlib figure object.
+        """
+        selected_trace = str(trace_index + 1)
+        num_data_pts = self.scan_data[0].shape[1]
+        time = np.arange(0, 16*num_data_pts, 16) # time in ns
+        time_ms = time / 1e6
+
+        raw_pressure_waveform = self.scan_data[0]
+
+        self.fig_time, self.ax_time = plt.subplots(figsize=(10, 6))
+        self.canvas_time = FigureCanvas(self.fig_time)
+
+        self.fig_fft, self.ax_fft = plt.subplots(figsize=(10, 6))
+        self.canvas_fft = FigureCanvas(self.fig_fft)
+        selected_waveform = raw_pressure_waveform.iloc[trace_index]
+
+        # time domain trace
+        self.ax_time.plot(
+            time_ms,
+            selected_waveform,
+            label=selected_trace,
+            color='#73A89E'
+            )
+        self.ax_time.set_xlabel("Time (ms)")
+        self.ax_time.set_ylabel("Pressure (Pa)")
+        self.ax_time.set_title("Pressure vs Time")
+        #print(f"Time graph of trace {selected_trace} plotted")
+
+        # frequency domain FFT
+        window_fft = np.hanning(num_data_pts)
+
+        real_fft_magnitude = np.fft.rfft(selected_waveform * window_fft, axis=0)
+
+        fft_wf = np.abs(2 * 2 * real_fft_magnitude / num_data_pts) / 1e6 # MPa
+        fft_freq_mhz = np.fft.rfftfreq(n=num_data_pts, d=self.time_delta) / 1e6 # MHz
+
+        self.ax_fft.plot(
+            fft_freq_mhz,
+            fft_wf,
+            label=selected_trace,
+            color='#73A89E'
+            )
+        self.ax_fft.set_xlabel("Frequency (MHz)")
+        self.ax_fft.set_ylabel("Pressure (MPa)")
+        self.ax_fft.set_title("Pressure vs Frequency")
+        self.ax_fft.set_xlim(0, 5)
+
+        return self.canvas_time, self.canvas_fft
--- sweep_plotter/sweep_plot_tab.py
+++ sweep_plotter/sweep_plot_tab.py
@@ -1,4 +1,4 @@
-from hydrophone.hydrophone_graph import HydrophoneGraph
+from sweep_plotter.sweep_graph import SweepGraph
 
 from PySide6.QtCore import Slot, Qt
 from PySide6.QtWidgets import (QCheckBox, QComboBox, QFileDialog, QHBoxLayout, QPushButton, QGridLayout, QGroupBox, 
@@ -8,6 +8,7 @@ import numpy as np
 import os
 import yaml
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
+import h5py
 
 class SweepGraphTab(QWidget):
     def __init__(self, parent=None) -> None:
@@ -25,26 +26,32 @@ class SweepGraphTab(QWidget):
         self.print_graph_btn = QPushButton("PRINT GRAPH")
         self.print_graph_btn.setStyleSheet("background-color: #74BEA3")
         self.print_graph_btn.clicked.connect(lambda: self.create_graph())
+        # spin box for trace number
+        self.trace_no_menu = QComboBox()
+        self.trace_no_menu.setEnabled(False)
+        self.trace_no_label = QLabel("Select Trace Number:")
 
         # Layout for user interaction area
         selections_layout = QGridLayout()
         selections_layout.addWidget(self.select_file_btn, 0, 0, 1, 2)
-        selections_layout.addWidget(self.print_graph_btn, 1, 0, 1, 2)
+        selections_layout.addWidget(self.trace_no_label, 1, 0)
+        selections_layout.addWidget(self.trace_no_menu, 1, 1)
+        selections_layout.addWidget(self.print_graph_btn, 2, 0, 1, 2)
         buttons_groupbox.setLayout(selections_layout)
+        buttons_groupbox.setFixedWidth(buttons_groupbox.minimumSizeHint().width())
 
         # TEXT CONSOLE
         self.text_display = QTextBrowser()
+        self.text_display.setFixedWidth(buttons_groupbox.minimumSizeHint().width())
 
         # GRAPH DISPLAY 
-        self.time_domain_tab = QTabWidget()
-        self.fft_tab = QTabWidget()
+        self.graph_tabs = QTabWidget()
 
         # MAIN LAYOUT
         main_layout = QGridLayout()
         main_layout.addWidget(buttons_groupbox, 0, 0)
         main_layout.addWidget(self.text_display, 1, 0)
-        main_layout.addWidget(self.time_domain_tab, 0, 1, 2, 1)
-        main_layout.addWidget(self.fft_tab, 0, 2, 2, 1)
+        main_layout.addWidget(self.graph_tabs, 0, 1, 2, 1)
         self.setLayout(main_layout)
     
     @Slot()
@@ -74,30 +81,53 @@ class SweepGraphTab(QWidget):
                 self.text_display.append("Save Location: ")
                 self.file_save_location = self.dialog.selectedFiles()[0]
                 self.text_display.append(self.file_save_location+"\n")
+
+        self.trace_no_menu.setEnabled(True)
+        self.trace_no_menu.clear()
+        with h5py.File(self.scan_data_hdf5[0], 'r') as f:
+            # extract the number of rows to set the drop down
+            scan_group = f['Scan']
+            raw_pressu_waveforms = scan_group['Raw pressure waveforms (Pa)'][:]
+            self.trace_no_menu.addItems([str(i+1) for i in range(raw_pressu_waveforms.shape[0])])
+            # set the max index
+            self.trace_no_menu.setCurrentIndex(raw_pressu_waveforms.shape[0]-1)
     
-    # @Slot()
-    # def create_graph(self):
-    #     if self.scan_data_hdf5 is not None:
-    #         self.time_domain_tab.clear()
+    @Slot()
+    def create_graph(self):
+        if self.scan_data_hdf5 is not None:
+            self.graph_tabs.clear()
 
-    #         scan_data_object = HydrophoneGraph(self.scan_data_hdf5)
-    #         graph = scan_data_object.get_graphs(self.trace_no.value()) 
+            scan_data_object = SweepGraph(self.scan_data_hdf5)
+            time_graph, fft_graph = scan_data_object.get_graphs(self.trace_no_menu.currentIndex(), graph_type='time')
             
-    #         nav_tool = NavigationToolbar(graph)
+            # if time_graph is None or fft_graph is None:
+            #     print("Error: One of the graph canvases is None")
+            #     return
 
-    #         graph_widget = QWidget()
-    #         burn_layout = QVBoxLayout()
-    #         burn_layout.addWidget(nav_tool)
-    #         burn_layout.addWidget(graph)
-    #         graph_widget.setLayout(burn_layout)
+            nav_tool_time = NavigationToolbar(time_graph)
+            nav_tool_fft = NavigationToolbar(fft_graph)
 
-    #         self.time_domain_tab.addTab(graph_widget, "Scan Data Time Domain")
+            time_widget = QWidget()
+            time_layout = QVBoxLayout()
+            time_layout.addWidget(nav_tool_time)
+            time_layout.addWidget(time_graph)
+            time_widget.setLayout(time_layout)
+            self.graph_tabs.addTab(time_widget, "Time Domain")
 
-    #         #self.text_display.append("Transducer Serial Number: " + scan_data_object.tx_serial_no + "\n")
-    #         # Debugging statements
-    #         # print(f"save_box is checked: {self.save_box.isChecked()}")
-    #         # if self.file_save_location is not None:
-    #         #   print(f"file_save_location: {self.file_save_location}")
+            fft_widget = QWidget()
+            fft_layout = QVBoxLayout()
+            fft_layout.addWidget(nav_tool_fft)
+            fft_layout.addWidget(fft_graph)
+            fft_widget.setLayout(fft_layout)
+            self.graph_tabs.addTab(fft_widget, "FFT Graph")
 
-    #     else:
-    #         self.text_display.append("Error: No hydrophone data .csv file found.\n")
+            # parts = self.scan_data_hdf5.split('_')
+            # tx_serial_no = "-".join(parts[:2])
+            # self.text_display.append("Transducer Serial Number: " + tx_serial_no + "\n")
+            # Debugging statements
+            # print(f"save_box is checked: {self.save_box.isChecked()}")
+            # if self.file_save_location is not None:
+            #   print(f"file_save_location: {self.file_save_location}")
+
+        else:
+            self.text_display.append("Error: No scan data hdf5 file found.\n")
